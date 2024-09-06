e42244fe9e3e69c1158967e28e0c264d348964b4
Federico Rodriguez <Federico0502001@gmail.com>
2024-09-06 03:00:53 -0300

    Pantalla creacion de usuario

--- app/src/main/java/com/example/worldbook/RegisterActivity.kt
+++ app/src/main/java/com/example/worldbook/RegisterActivity.kt
@@ -1,12 +1,25 @@
-package com.example.worldbook
+package com.example.intentologin
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.worldbook.R
 
 class RegisterActivity : AppCompatActivity() {
+
+    lateinit var btnCrearUsuario: Button
+    lateinit var etNombreUsuario: EditText
+    lateinit var etPassword: EditText
+    lateinit var etPasswordAgain: EditText
+    lateinit var etEmail:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +29,32 @@ class RegisterActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        btnCrearUsuario=findViewById(R.id.btnCrearUsuario)
+        etNombreUsuario=findViewById(R.id.etNombreUsuario)
+        etPassword=findViewById(R.id.etPassword)
+        etPasswordAgain=findViewById(R.id.etPasswordAgain)
+        etEmail=findViewById(R.id.etEmail)
+
+
+        btnCrearUsuario.setOnClickListener {
+            var email=etEmail.text.toString()
+            var usuario=etNombreUsuario.text.toString()
+            var password=etPassword.text.toString()
+            var passwordAgain=etPasswordAgain.text.toString()
+
+            if (email.isEmpty() || usuario.isEmpty() || password.isEmpty() || passwordAgain.isEmpty() ){
+                var mensaje = "Complete todos los campos"
+                Toast.makeText(this,mensaje,Toast.LENGTH_SHORT).show()
+            }else{
+                if(password!=passwordAgain){
+                    Toast.makeText(this,"Contrasenia no coincide",Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this,"Contrasenia coinciden",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
+
+
 }
