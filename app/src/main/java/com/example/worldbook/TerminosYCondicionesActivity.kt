786194528c90f8e1bef4a46f12472b0cc9735c01
Francisco <gautobattista@gmail.com>
2024-09-07 13:20:38 -0300

    Creacion de la pantalla "terminos y condiciones"

--- app/src/main/java/com/example/worldbook/TerminosYCondicionesActivity.kt
+++ app/src/main/java/com/example/worldbook/TerminosYCondicionesActivity.kt
@@ -1,20 +1,40 @@
 package com.example.worldbook
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class TerminosYCondicionesActivity : AppCompatActivity() {
+    lateinit var btnAceptarTyC: Button
+    lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_terminos_ycondiciones)
+
+        // Ajuste del padding para las barras del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+
+        // Configurar botón aceptar
+        btnAceptarTyC = findViewById(R.id.btn_aceptar_tyc)
+        btnAceptarTyC.setOnClickListener {
+            Log.i("TODO", "Se aceptan los términos y condiciones")
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
