d3bdf511dfd32c586c24cb981f9dc98a3b1531ba
Francisco <gautobattista@gmail.com>
2024-10-20 19:19:08 -0300

    Vinculo el toolbar en "favoritos"

--- app/src/main/java/com/example/worldbook/FavoritosActivity.kt
+++ app/src/main/java/com/example/worldbook/FavoritosActivity.kt
@@ -1,28 +1,68 @@
 package com.example.worldbook
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class FavoritosActivity : AppCompatActivity() {
     private lateinit var recyclerFavoritos: RecyclerView
     private lateinit var favoritosAdapter: LibroAdapter
-
-
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_favoritos)
 
+        // Inicio toolbar
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Opciones"
 
+        // Ajustar el padding de la vista principal
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Configurar el RecyclerView
         recyclerFavoritos = findViewById(R.id.recyclerFavoritos)
         recyclerFavoritos.layoutManager = LinearLayoutManager(this)
 
-        // Obtener la lista de libros favoritos desde donde se almacenen (SharedPreferences, etc.)
+        // Obtener la lista de libros favoritos desde donde se almacenen
         val librosFavoritos = FavoritosManager.getFavoritos()
-
         favoritosAdapter = LibroAdapter(librosFavoritos)
         recyclerFavoritos.adapter = favoritosAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_mi_biblioteca -> {
+                startActivity(Intent(this, MiBibliotecaActivity::class.java))
+                true
+            }
+            R.id.item_cerrar_sesion -> {
+                val preferencias = getSharedPreferences(resources.getString(R.string.sp_credenciales), MODE_PRIVATE)
+                preferencias.edit().clear().apply()
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
